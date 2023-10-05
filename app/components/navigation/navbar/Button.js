fc369dccaab2570978f0bdc4063827cc4ee6f3aa
Derek Malone <laxmalone@gmail.com>
2023-10-05 17:07:16 -0500

    added most file structure, got a bit carried away with Navbar...

---
+++ app/components/navigation/navbar/Button.js
@@ -0,0 +1,7 @@
+const Button = () => {
+  // TODO: Need to determine if I plan to use this at all.
+  return (
+    <button className='h-12 rounded-lg bg-white font-bold px-5'>Sign In</button>
+  );
+};
+export default Button;
