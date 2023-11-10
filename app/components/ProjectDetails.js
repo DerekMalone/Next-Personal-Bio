814dc0f75a3d3e7440b8bb4bd731dc4f33a9ae08
Derek Malone <laxmalone@gmail.com>
2023-11-09 20:28:46 -1000

    made card for project

--- app/components/ProjectDetails.js
+++ app/components/ProjectDetails.js
@@ -1,41 +1,58 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
 import getProject from "./helpers/data";
 
 const ProjectDetails = ({ projectObj }) => {
   const [project, setProject] = useState({});
+  const router = useRouter()
 
   useEffect(() => {
     getProject(projectObj).then(setProject);
   }, []);
 
-  return (
-    <section className='border-solid border-2 border-indigo-600 p-3 m-3'>
-      <article className='project-detail'>
-        <h2 className='name-style'>{project.name}</h2>
-        {/* <h3 className='projects-style'>{projects[7]}</h3> */}
+  const routeNavigate = () => {
+    router.push(`${project.html_url}`)
+  }
 
-        {/* <div className='card-btn-container'>
-          <a href={projects[6]} type='button' className='btn btn-link'>
-            Go to Project
-          </a>
-          <Link
-            to={`/edit/${project.firebaseKey}`}
-            type='button'
-            className='btn btn-success'
-          >
-            Edit
-          </Link>
-          <button
-            type='button'
-            className='btn btn-outline-danger'
-            onClick={handleDelete}
-          >
-            Delete
-          </button>
-        </div> */}
-      </article>
-    </section>
+  return (
+    
+<section class="m-3 mx-auto max-w-md rounded-lg bg-white shadow">
+  <article class="p-4">
+    <h3 class="text-xl font-medium text-gray-900">Migrating to Sailboat UI: {project.name}</h3>
+    <p class="mt-1 text-gray-500">{project.description}</p>
+    <div className="flex justify-center">
+    <button type="button" className="content-cetner bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => routeNavigate()}>Link: {project.name}</button>
+    </div>
+  </article>
+</section>
+
+    // <section className='border-solid border-2 border-indigo-600 p-3 m-3'>
+    //   <article className='project-detail'>
+    //     <h2 className='name-style'>{project.name}</h2>
+    //     {/* <h3 className='projects-style'>{projects[7]}</h3> */}
+
+    //     {/* <div className='card-btn-container'>
+    //       <a href={projects[6]} type='button' className='btn btn-link'>
+    //         Go to Project
+    //       </a>
+    //       <Link
+    //         to={`/edit/${project.firebaseKey}`}
+    //         type='button'
+    //         className='btn btn-success'
+    //       >
+    //         Edit
+    //       </Link>
+    //       <button
+    //         type='button'
+    //         className='btn btn-outline-danger'
+    //         onClick={handleDelete}
+    //       >
+    //         Delete
+    //       </button>
+    //     </div> */}
+    //   </article>
+    // </section>
   );
 };
 
