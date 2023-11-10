a4465cf85b62fa66037fba98e88f79338d5fa3de
Derek Malone <laxmalone@gmail.com>
2023-11-09 20:30:47 -1000

    removed zombie code

--- app/components/ProjectDetails.js
+++ app/components/ProjectDetails.js
@@ -27,32 +27,6 @@ const ProjectDetails = ({ projectObj }) => {
   </article>
 </section>
 
-    // <section className='border-solid border-2 border-indigo-600 p-3 m-3'>
-    //   <article className='project-detail'>
-    //     <h2 className='name-style'>{project.name}</h2>
-    //     {/* <h3 className='projects-style'>{projects[7]}</h3> */}
-
-    //     {/* <div className='card-btn-container'>
-    //       <a href={projects[6]} type='button' className='btn btn-link'>
-    //         Go to Project
-    //       </a>
-    //       <Link
-    //         to={`/edit/${project.firebaseKey}`}
-    //         type='button'
-    //         className='btn btn-success'
-    //       >
-    //         Edit
-    //       </Link>
-    //       <button
-    //         type='button'
-    //         className='btn btn-outline-danger'
-    //         onClick={handleDelete}
-    //       >
-    //         Delete
-    //       </button>
-    //     </div> */}
-    //   </article>
-    // </section>
   );
 };
 
